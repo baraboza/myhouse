1848aea3c275bb1de44635b2d04d11ff74bf9080
Maxim Tkachev <maximtkachev@iMac-Maxim.local>
2019-07-09 17:55:58 +0400

    bxslider поставил, не очень, попробую снова slick

--- app/js/common.js
+++ app/js/common.js
@@ -30,32 +30,48 @@ $('input[type=tel]').mask('+7 (999) 999-9999');
 
 
 // reviews
-	$('.reviews__item').eq(0).addClass('reviews__item--focus');
-	var reviewsItemWidth = $('.reviews__item--focus').css('width').replace('px', '');
-	var reviewsIndex = 0;
-	var reviewsMaxIndex = $('.reviews__item').length - 1;
+	// $('.reviews__item').eq(0).addClass('reviews__item--focus');
+	// var reviewsItemWidth = $('.reviews__item--focus').css('width').replace('px', '');
+	// var reviewsIndex = 0;
+	// var reviewsMaxIndex = $('.reviews__item').length - 1;
 	
-	for (var i = 0; i <= reviewsMaxIndex; i++) {
-		$('.reviews__dots').append('<span data-index="' + i + '" class="reviews__dots-item"></span>')
-	}
-	$('.reviews__dots-item').eq(0).addClass('reviews__dots-item--active');
+	// for (var i = 0; i <= reviewsMaxIndex; i++) {
+	// 	$('.reviews__dots').append('<span data-index="' + i + '" class="reviews__dots-item"></span>')
+	// }
+	// $('.reviews__dots-item').eq(0).addClass('reviews__dots-item--active');
 
-	$('.reviews__button--next').click(function() {
-		if (reviewsIndex < reviewsMaxIndex) {
-			reviewsIndex++;
-			$('.reviews__list').css('left', (-reviewsIndex * reviewsItemWidth) + 'px')
-			$('.reviews__item--focus').next().addClass('reviews__item--focus');
-			$('.reviews__item--focus').eq(0).removeClass('reviews__item--focus');
-		}
-	});
+	// $('.reviews__button--next').click(function() {
+	// 	if (reviewsIndex < reviewsMaxIndex) {
+	// 		reviewsIndex++;
+	// 		$('.reviews__list').css('left', (-reviewsIndex * reviewsItemWidth) + 'px')
+	// 		$('.reviews__item--focus').next().addClass('reviews__item--focus');
+	// 		$('.reviews__item--focus').eq(0).removeClass('reviews__item--focus');
+	// 	}
+	// });
 
-	$('.reviews__button--prev').click(function() {
-		if (reviewsIndex > 0) {
-			reviewsIndex--;
-			$('.reviews__list').css('left', (-reviewsIndex * reviewsItemWidth) + 'px')
-			$('.reviews__item--focus').prev().addClass('reviews__item--focus');
-			$('.reviews__item--focus').eq(1).removeClass('reviews__item--focus');
-		}
+	// $('.reviews__button--prev').click(function() {
+	// 	if (reviewsIndex > 0) {
+	// 		reviewsIndex--;
+	// 		$('.reviews__list').css('left', (-reviewsIndex * reviewsItemWidth) + 'px')
+	// 		$('.reviews__item--focus').prev().addClass('reviews__item--focus');
+	// 		$('.reviews__item--focus').eq(1).removeClass('reviews__item--focus');
+	// 	}
+	// });
+
+	// $('.reviews__list').append('<li></li>');
+	
+	// $('.reviews__list').slick({
+	// 	prevArrow: $('.reviews__button--prev'),
+	// 	nextArrow: $('.reviews__button--next'),
+	// 	infinite: false
+	// });
+
+	$('.reviews__list').bxSlider({
+		infiniteLoop: false,
+		prevSelector: '.reviews__button--prev',
+		nextSelector: '.reviews__button--next',
+		prevText: '',
+		nextText: ''
 	});
 
 
